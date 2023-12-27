c7af5c65c6a796447af026a5de05c014fcf2658b
stephanie <stephaniewan07@gmail.com>
2023-12-26 23:14:01 -0500

    desc

--- services/index.js
+++ services/index.js
@@ -147,7 +147,10 @@ export const getAdjacentPosts = async (createdAt, slug) => {
 export const getCategoryPost = async (slug) => {
   const query = gql`
     query GetCategoryPost($slug: String!) {
-      postsConnection(where: {categories_some: {slug: $slug}}) {
+      postsConnection(
+        where: {categories_some: {slug: $slug}}
+        orderBy: createdAt_DESC
+        ) {
         edges {
           cursor
           node {
